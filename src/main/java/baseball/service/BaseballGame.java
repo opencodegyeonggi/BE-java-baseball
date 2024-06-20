2da8d0de5c4c42523d3518e5c2a879665c30f4fb
jake <epikeran@gmail.com>
2024-06-20 21:13:43 +0900

    refactor: try-with-resources 사용
    scanner 를 try-with-resources 사용하여 전달하는 것으로 바꿨습니다.

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -5,38 +5,40 @@ import baseball.domain.Baseballs;
 import baseball.ui.Input;
 import baseball.ui.Output;
 
+import java.util.Scanner;
+
 public class BaseballGame {
     private final Computer computer = new Computer();
     private Baseballs userBaseballs;
     private Baseballs computerBaseballs;
 
-    public void playBaseball() {
+    public void playBaseball(Scanner scanner) {
         computerBaseballs = computer.generateRandomBaseballs();
 
         boolean correct = false;
 
         while(!correct) {
-            playBaseballOnce();
+            playBaseballOnce(scanner);
             correct = isCorrectAnswer();
         }
 
         Output.printCorrectAnswer();
     }
 
-    private void playBaseballOnce() {
-        userBaseballs = receiveUserBaseballs();
+    private void playBaseballOnce(Scanner scanner) {
+        userBaseballs = receiveUserBaseballs(scanner);
         Result result = new Result(computerBaseballs, userBaseballs);
 
         Output.printBaseballGameResult(result.getResult());
     }
 
-    private Baseballs receiveUserBaseballs() {
+    private Baseballs receiveUserBaseballs(Scanner scanner) {
         try {
-            return BaseballFactory.createBaseballs(Input.receiveNumberInput());
+            return BaseballFactory.createBaseballs(Input.receiveNumberInput(scanner));
         } catch (IllegalArgumentException e) {
             Output.printExceptionMessage(e.getMessage());
 
-            return receiveUserBaseballs();
+            return receiveUserBaseballs(scanner);
         }
     }
 
